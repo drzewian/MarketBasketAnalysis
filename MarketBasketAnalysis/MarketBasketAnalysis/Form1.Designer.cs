c02caedd16734eb5b1628d55dea2d8ca88a735b3
kxj634 <kxj634@PC0981KX>
2016-10-18 21:52:05 +0200

    First code.

--- MarketBasketAnalysis/MarketBasketAnalysis/Form1.Designer.cs
+++ MarketBasketAnalysis/MarketBasketAnalysis/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(127, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(108, 199);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(241, 13);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(108, 199);
+            this.listBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 262);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- MarketBasketAnalysis/MarketBasketAnalysis/Form1.cs
+++ MarketBasketAnalysis/MarketBasketAnalysis/Form1.cs
@@ -16,5 +16,52 @@ namespace MarketBasketAnalysis
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, List<string>> transactions = new Dictionary<int, List<string>>();
+            List<string> lista = new List<string> { "1", "3", "4" };
+            List<string> lista1 = new List<string> { "2", "3", "5" };
+            List<string> lista2 = new List<string> { "1", "2", "3", "5" };
+            List<string> lista3 = new List<string> { "2", "5" };
+            List<string> lista4 = new List<string> { "1", "3", "5" };
+            transactions.Add(0, lista);
+            transactions.Add(1, lista1);
+            transactions.Add(2, lista2);
+            transactions.Add(3, lista3);
+            transactions.Add(4, lista4);
+
+            int minimumSupportCount = 2;
+
+            Dictionary<string, int> candidates1 = new Dictionary<string, int>();
+
+            foreach (var transaction in transactions)
+            {
+                foreach (var item in transaction.Value)
+                {
+                    if (candidates1 != null && candidates1.Keys.Contains(item))
+                    {
+                        candidates1[item] += 1;
+                        continue;
+                    }
+
+                    candidates1.Add(item, 1);
+                }
+            }
+
+            //foreach (var item in candidates1)
+            //{
+            //    if (item.Value < minimumSupportCount)
+            //    {
+            //        candidates1.Remove(item.Key);
+            //    }
+            //}
+
+            foreach (var item in candidates1)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
     }
 }
